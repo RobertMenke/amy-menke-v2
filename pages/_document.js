8d823cf4ad3726cca84cacbf33646152e9455e86
Robert Menke <robert.b.menke@gmail.com>
2021-04-30 22:44:46 -0400

    fix: crossOrigin attribute

--- pages/_document.js
+++ pages/_document.js
@@ -49,7 +49,7 @@ class MyDocument extends Document {
                     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
                     <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
                     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-                    <link rel="preconnect" href="https://rsms.me" crossorigin />
+                    <link rel="preconnect" href="https://rsms.me" crossOrigin />
                     <link rel="preload" as="style" href="https://rsms.me/inter/inter.css" />
 
                     <link
