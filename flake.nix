fc2c97a0ef2e5e3bd30edc93435ecae4ed46ddd1
Robert Menke <robert.b.menke@gmail.com>
2026-04-19 20:34:19 -0400

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "amy-redesign – Next.js development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_18
+            corepack_18
+          ];
+        };
+      });
+}
